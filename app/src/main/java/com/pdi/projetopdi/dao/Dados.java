b34f89054bda3e54a9d1294927fbd4c4f3712686
larita1978 <larissainesdemorais@hotmail.com>
2020-05-15 12:54:17 -0300

    Criado tela Principal e algumas coisas de SQLite

---
+++ app/src/main/java/com/pdi/projetopdi/dao/Dados.java
@@ -0,0 +1,4 @@
+package com.pdi.projetopdi.dao;
+
+public class Dados {
+}
---
+++ app/src/main/java/com/pdi/projetopdi/ui/activity/ListaProdutosActivity.java
@@ -0,0 +1,13 @@
+package com.pdi.projetopdi.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaProdutosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_produtos);
+    }
+}
