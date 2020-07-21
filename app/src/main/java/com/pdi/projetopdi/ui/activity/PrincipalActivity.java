982e49c1c0f065ebc7f9e36ce4becbedf39db09b
larita1978 <larissainesdemorais@hotmail.com>
2020-07-21 17:41:15 -0300

    design da tela principal quase pronto!

--- app/src/main/java/com/pdi/projetopdi/ui/activity/PrincipalActivity.java
+++ app/src/main/java/com/pdi/projetopdi/ui/activity/PrincipalActivity.java
@@ -16,6 +16,7 @@ public class PrincipalActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_principal);
 
         Button botaoDirecionaPedido = findViewById(R.id.btPedido);
@@ -32,8 +33,8 @@ public class PrincipalActivity extends AppCompatActivity {
         botaoDirecionaPedido.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(PrincipalActivity.this,"Clicou botão produto",Toast.LENGTH_SHORT);
-                startActivity(new Intent(PrincipalActivity.this,ListaProdutosActivity.class));
+                Toast.makeText(PrincipalActivity.this,"Clicou botão pedido",Toast.LENGTH_SHORT);
+                startActivity(new Intent(PrincipalActivity.this,ListaPedidos.class));
             }
         });
     }
