ffd8ba1f3898d5e619756ddf276ed2b343cdb1e4
larita1978 <larissainesdemorais@hotmail.com>
2020-08-20 23:04:19 -0300

    fragments

--- app/src/main/java/com/pdi/projetopdi/fragments/PageFragmentPedidoCarrinho.java
+++ app/src/main/java/com/pdi/projetopdi/fragments/PageFragmentPedidoCarrinho.java
@@ -1,9 +1,13 @@
 package com.pdi.projetopdi.fragments;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,7 +19,26 @@ public class PageFragmentPedidoCarrinho extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.page_itens, container, false);
+        final ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.page_itens, container, false);
+
+        Button btAddProduto = rootView.findViewById(R.id.btAddProduto);
+
+        btAddProduto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAndShowDialog();
+            }
+        });
         return rootView;
+    }
+
+    public void createAndShowDialog() {
+        Dialog dialog = new Dialog();
+        dialog.setContentView(R.layout.dialog_add_editar_produto_pedido);
+
+        WindowManager.LayoutParams layoutParams = dialog.getWindow().getAttributes();
+        dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+        dialog.getWindow().setAttributes(layoutParams);
+        dialog.show();
     }
 }
